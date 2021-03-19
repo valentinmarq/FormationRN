9b791cb5c6e184f1e9f9daa35605bd9e512789da
Valentin Marquis <valentinm@bam.tech>
2021-03-19 16:07:08 +0100

    Add research Button in App.tsx

--- AwesomeTSProject/App.tsx
+++ AwesomeTSProject/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {SafeAreaView, TextInput} from 'react-native';
+import {SafeAreaView, TextInput, Button} from 'react-native';
 
 const App = () => {
   return (
     <SafeAreaView>
       <TextInput placeholder="Titre de la chanson" />
+      <Button title="Rechercher" onPress={() => {}} />
     </SafeAreaView>
   );
 };
